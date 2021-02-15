6cdb2f26b2a67965529ba1d584577b4462f29759
EuJin03 <eugenetin99@gmail.com>
2021-02-15 22:09:24 +0800

    -updates: bug fixes

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ import { config } from "dotenv";
 config();
 import colors from "colors";
 
-import { ApolloServer, PubSub } from "apollo-server";
+import { ApolloServer, PubSub } from "./graphql/node_modules/apollo-server";
 import connectDB from "./config/db.js";
 import { typeDefs } from "./graphql/typeDefs.js";
 import resolvers from "./graphql/resolvers/index.js";
@@ -10,20 +10,6 @@ import resolvers from "./graphql/resolvers/index.js";
 connectDB();
 
 const pubsub = new PubSub();
-
-const __dirname = path.resolve();
-
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "/client/build")));
-
-  app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname, "client", "build", " index.html"))
-  );
-} else {
-  app.get("/", (req, res) => {
-    res.send("API is running");
-  });
-}
 
 const server = new ApolloServer({
   typeDefs: typeDefs,
